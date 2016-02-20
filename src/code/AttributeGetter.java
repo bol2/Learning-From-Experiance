576523f344166e8bb358783900b5bfa641358503
bol2 <bol2@aber.ac.uk>
2016-02-20 12:24:17 +0000

    Updated Attribute Getter Method
    Created a method to return a numerical value of attribute

--- src/code/AttributeGetter.java
+++ src/code/AttributeGetter.java
@@ -1,27 +1,55 @@
 package code;
+import java.util.ArrayList;
 
 public class AttributeGetter {
-	
+
 	Attribute A;
-	
-	public AttributeGetter(Attribute A){
+
+	public AttributeGetter(Attribute A) {
 		this.A = A;
 	}
-	
-	public int getAttribute(){
-		switch (A){
-			case AGE:
-				return 1;
-			case PERSCRIPTION:
-				return 2;
-			case ASTIGMATIC:
-				return 3;
-			case TEARPRODRATE:
-				return 4;
-			
-			default:
-				System.out.println("Undefined Attribute");
-				return 0;	
+
+	public int getAttribute() {
+		switch (A) {
+		case AGE:
+			return 0;
+		case PERSCRIPTION:
+			return 1;
+		case ASTIGMATIC:
+			return 2;
+		case TEARPRODRATE:
+			return 3;
+
+		default:
+			System.out.println("Undefined Attribute");
+			return 0;
+		}
+	}
+
+	public ArrayList<Integer> getAttributeValues() {
+		ArrayList<Integer> list = new ArrayList<>();
+		switch (A) {
+		case AGE:
+			list.add(1);
+			list.add(2);
+			list.add(3);
+			return list;
+		case PERSCRIPTION:
+			list.add(1);
+			list.add(2);
+			return list;
+		case ASTIGMATIC:
+			list.add(1);
+			list.add(2);
+			return list;
+		case TEARPRODRATE:
+			list.add(1);
+			list.add(2);
+			return list;
+
+		default:
+			System.out.println("Undefined Attribute");
+			return list;
 		}
 	}
 
